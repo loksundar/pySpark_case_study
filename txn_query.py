f94e40893e7bd927bdc8ce60127e76aa38ccf160
Loksundar Ganthi <loksundar000@gmail.com>
2022-03-21 17:18:43 +0530

    mostly done

--- txn_query.py
+++ txn_query.py
@@ -29,6 +29,40 @@ df = spark.sql("""select day,count(visit_number) as num_of_visits from txn group
 df.write.format('bigquery') \
   .option('table', 'retail-immersion:bqtest.visits_by_weekday') \
   .save()
+df = spark.sql("""select channel,count(visit_number) num_of_visits from txn group by channel order by num_of_visits desc""")
+df.write.format('bigquery') \
+  .option('table', 'retail-immersion:bqtest.visits_per_channel') \
+  .save()
+df = spark.sql("""select day,channel,count(visit_number) num_of_visits from txn group by day,channel order by num_of_visits desc""")
+df.write.format('bigquery') \
+  .option('table', 'retail-immersion:bqtest.visits_per_channel_per_day') \
+  .save()
+df = spark.sql("""select product_brand,count(txn) num_of_txns from txn group by product_brand order by num_of_txns desc""")
+df.write.format('bigquery') \
+  .option('table', 'retail-immersion:bqtest.trans_on_brands') \
+  .save()
+df = spark.sql("""select a.product_brand, a.total_revenue/b.total_qty unit_price from 
+((select product_brand,sum(revenue) total_revenue from txn
+group by product_brand
+order by total_revenue desc) a inner join
+(select product_brand,sum(qty) total_qty from txn
+group by product_brand
+order by total_qty desc) b on a.product_brand = b.product_brand)""")
+df.write.format('bigquery') \
+  .option('table', 'retail-immersion:bqtest.quant_ship/sold_per_brand') \
+  .save()
+df = spark.sql("""select day,sum(revenue) total_revenue from txn group by day order by total_revenue desc""")
+df.write.format('bigquery') \
+  .option('table', 'retail-immersion:bqtest.rev_per_day') \
+  .save()
+df = spark.sql("""select channel,count(visit_number) num_of_visits from txn where visit_number=1 group by channel order by num_of_visits desc""")
+df.write.format('bigquery') \
+  .option('table', 'retail-immersion:bqtest.cust_channel_at_firstvisit') \
+  .save()
+df = spark.sql("""select channel,sum(revenue) total_revenue from txn group by channel order by total_revenue desc;""")
+df.write.format('bigquery') \
+  .option('table', 'retail-immersion:bqtest.rev_per_channel') \
+  .save()
 
 
 
