7045436fd0e2d88800f227d988b5800139bfdd64
Loksundar Ganthi <loksundar000@gmail.com>
2022-03-22 17:22:11 +0530

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -14,12 +14,7 @@ spark.conf.set('temporaryGcsBucket', bucket)
 
 # Create data frame
 file_path = "gs://retail_immersion_raw/"
-key_df = spark.read.csv(file_path+"Keywords data.csv",header=True)
-chp_df = spark.read.csv(file_path+"Channel Performance.csv",header=True)
 txn_df = spark.read.csv(file_path+"TXN Data.csv",header=True)
-key_df = key_df.withColumnRenamed("Bounce Rate",'Bounce_Rate')
-key_df = key_df.withColumnRenamed('Pages / Session','Pages_per_Session')
-chp_df = chp_df.withColumnRenamed('Landing Page','Landing_Page')
 """
 # Saving the data to BigQuery
 key_df.write.format('bigquery') \
@@ -39,23 +34,10 @@ txn_df = txn_df.withColumn('date_time',to_timestamp(col("date_of_visit")))
 txn_df = txn_df.withColumn('day',date_format(col("date_of_visit"), "EEEE"))
 client = bigquery.Client()
 txn_df.createOrReplaceTempView("txn")
-# Delete after one run 
-table_id = 'retail-immersion.bqtest.visits_per_channel_per_day'
-client.delete_table(table_id, not_found_ok=True)
-table_id = 'retail-immersion.bqtest.visits_per_channel'
-client.delete_table(table_id, not_found_ok=True)
-table_id = 'retail-immersion.bqtest.visits_by_weekday'
-client.delete_table(table_id, not_found_ok=True)
-table_id = 'retail-immersion.bqtest.rev_per_day'
-client.delete_table(table_id, not_found_ok=True)
-table_id = 'retail-immersion.bqtest.rev_per_channel'
-client.delete_table(table_id, not_found_ok=True)
-####
 
-
-table_id = 'retail-immersion.bqtest.Table2'
+table_id = 'retail-immersion.bqtest.TXN_Table2'
 client.delete_table(table_id, not_found_ok=True)
-df = df = spark.sql("""select a.product_brand,a.num_of_txns,a.total_revenue/b.total_qty unit_price from 
+df = spark.sql("""select a.product_brand,a.num_of_txns,a.total_revenue/b.total_qty unit_price from 
 ((select product_brand,count(txn) num_of_txns ,sum(revenue) total_revenue from txn
 group by product_brand
 order by total_revenue desc) a inner join
@@ -63,7 +45,7 @@ order by total_revenue desc) a inner join
 group by product_brand
 order by total_qty desc) b on a.product_brand = b.product_brand)""")
 df.write.format('bigquery') \
-  .option('table', 'retail-immersion:bqtest.Table2') \
+  .option('table', 'retail-immersion:bqtest.TXN_Table2') \
   .save()
 
 table_id = 'retail-immersion.bqtest.cust_channel_at_firstvisit'
@@ -73,28 +55,27 @@ df.write.format('bigquery') \
   .option('table', 'retail-immersion:bqtest.cust_channel_at_firstvisit') \
   .save()
 
-table_id = 'retail-immersion.bqtest.test0'
+table_id = 'retail-immersion.bqtest.TXN_Table1'
 client.delete_table(table_id, not_found_ok=True)
 df = spark.sql("""select channel , day , count(qty) as total_units , sum(revenue) as total_Revenue,count(visit_number) as num_of_visits , sum(txn) as no_of_txns from txn 
 group by grouping sets ((channel,day),(channel),(day),()) order by channel""")
 df = df.na.fill("ALL")
 df.write.format('bigquery') \
-  .option('table', 'retail-immersion:bqtest.test0') \
+  .option('table', 'retail-immersion:bqtest.TXN_Table1') \
   .save()
+#####################################################################################################################################
+chp_df = spark.read.csv(file_path+"Channel Performance.csv",header=True)
+chp_df = chp_df.withColumnRenamed('Landing Page','Landing_Page')
+chp_df = chp_df.na.fill(value="0")
+chp_df.createOrReplaceTempView("chn")
+table_id = 'retail-immersion.bqtest.CHN_Table1'
+client.delete_table(table_id, not_found_ok=True)
+df = spark.sql("select channel, sum(bounces) as bounces , sum(txn) as no_of_txn,sum(revenue) as total_revenue, count(Cost) as Total_Cost from chn group by channel")
+df.write.format('bigquery') \
+  .option('table', 'retail-immersion:bqtest.CHN_Table1') \
+  .save()
+#####################################################################################################################################
+key_df = spark.read.csv(file_path+"Keywords data.csv",header=True)
+key_df = key_df.withColumnRenamed("Bounce Rate",'Bounce_Rate')
+key_df = key_df.withColumnRenamed('Pages / Session','Pages_per_Session')
 
-"""
-table_id = 'retail-immersion.bqtest.final_table0'
-client.delete_table(table_id, not_found_ok=True)
-df = spark.sql("select channel , day , count(qty) as total_units , sum(revenue) as total_Revenue from txn group by grouping sets ((channel,day),(channel),(day),()) order by channel")
-df = df.na.fill("ALL")
-df.write.format('bigquery') \
-  .option('table', 'retail-immersion:bqtest.final_table0') \
-  .save()
-table_id = 'retail-immersion.bqtest.final_table1'
-client.delete_table(table_id, not_found_ok=True)
-df = spark.sql("select channel , day ,count(visit_number) as num_of_visits , sum(txn) as no_of_txns from txn group by grouping sets ((channel,day),(channel),(day),()) order by channel")
-df = df.na.fill("ALL")
-df.write.format('bigquery') \
-  .option('table', 'retail-immersion:bqtest.final_table1') \
-  .save()
-  """
